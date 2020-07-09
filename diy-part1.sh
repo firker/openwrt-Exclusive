99e27cadbefbaa30f54e20dae658e3452b614064
firker <56651759+firker@users.noreply.github.com>
2020-07-09 16:40:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,5 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i '$a src-git diy https://github.com/firker/diy-ziyong' feeds.conf.default
 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 
-git clone https://github.com/tty228/luci-app-serverchan.git
+#git clone https://github.com/tty228/luci-app-serverchan.git
+#git clone https://github.com/destan19/OpenAppFilter.git
