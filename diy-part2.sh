f28e0630094c5062460664ef9b0c35faa98d25ec
firker <56651759+firker@users.noreply.github.com>
2020-07-10 19:20:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,6 +11,8 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-cp -r feeds/lienol/lienol/luci-app-passwall feeds/diy
-cp -r feeds/lienol/package/{chinadns-ng,trojan-go,syncthing,tcping} feeds/diy/vssr
-rm -rf ./feeds/{lienol,lienol.tmp}
+# cp -r feeds/lienol/lienol/luci-app-passwall feeds/diy
+# cp -r feeds/lienol/package/{chinadns-ng,trojan-go,syncthing,tcping} feeds/diy/vssr
+# rm -rf ./feeds/{lienol,lienol.tmp}
+rm -rf ./feeds/diy/luci-app-passwall
+rm -rf ./feeds/diy/vssr/{chinadns-ng,trojan-go,syncthing,tcping}
