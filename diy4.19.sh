b1ace779dcdadf3b9a9455b521d27680820fce57
firker <56651759+firker@users.noreply.github.com>
2020-07-24 16:59:51 +0800

    Update diy4.19.sh

--- diy4.19.sh
+++ diy4.19.sh
@@ -21,8 +21,10 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i '$a src-git diy https://github.com/firker/diy-ziyong' feeds.conf.default
 
 git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+
 svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-passwall
 svn co https://github.com/Lienol/openwrt-package/trunk/package/brook package/brook
 svn co https://github.com/Lienol/openwrt-package/trunk/package/chinadns-ng package/chinadns-ng
