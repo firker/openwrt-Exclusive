33b6c338a2044870c2ee66324e4cb3346cfb9715
firker <56651759+firker@users.noreply.github.com>
2020-08-07 00:00:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,10 +24,10 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luc
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFiltermkdir package/OpenAppFiltermkdir
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-passwall
-svn co https://github.com/Lienol/openwrt-package/trunk/package/brook package/brook
-svn co https://github.com/Lienol/openwrt-package/trunk/package/chinadns-ng package/chinadns-ng
-svn co https://github.com/Lienol/openwrt-package/trunk/package/tcping package/tcping
-svn co https://github.com/Lienol/openwrt-package/trunk/package/trojan-go package/trojan-go
-svn co https://github.com/Lienol/openwrt-package/trunk/package/trojan-plus package/trojan-plus
-svn co https://github.com/Lienol/openwrt-package/trunk/package/syncthing package/syncthing
+svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-passwall
+svn co https://github.com/xiaorouji/openwrt-package/trunk/package/brook package/brook
+svn co https://github.com/xiaorouji/openwrt-package/trunk/package/chinadns-ng package/chinadns-ng
+svn co https://github.com/xiaorouji/openwrt-package/trunk/package/tcping package/tcping
+svn co https://github.com/xiaorouji/openwrt-package/trunk/package/trojan-go package/trojan-go
+svn co https://github.com/xiaorouji/openwrt-package/trunk/package/trojan-plus package/trojan-plus
+svn co https://github.com/xiaorouji/openwrt-package/trunk/package/syncthing package/syncthing
