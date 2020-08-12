74602ba639c7fa5901dace8f7efd268f232485bc
firker <56651759+firker@users.noreply.github.com>
2020-08-12 21:30:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 rm -rf ./package/lean/luci-theme-argon
-
+rm -rf ./feeds/packages/net/smartdns
 # cp -r feeds/lienol/lienol/luci-app-passwall feeds/diy
 # cp -r feeds/lienol/package/{chinadns-ng,trojan-go,syncthing,tcping} feeds/diy/vssr
 # rm -rf ./feeds/{lienol,lienol.tmp}
