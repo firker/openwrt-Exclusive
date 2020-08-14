070f448f0b8edcd1440401abf41d7ce26a650788
firker <56651759+firker@users.noreply.github.com>
2020-08-14 23:48:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,7 +7,8 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #============================================================
-
+sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 rm -rf ./package/lean/luci-theme-argon
