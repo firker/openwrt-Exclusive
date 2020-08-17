2f1d828285980e30daedc392c38d24ba86d06c14
firker <56651759+firker@users.noreply.github.com>
2020-08-17 12:05:49 +0800

    Update project.sh

--- project.sh
+++ project.sh
@@ -12,9 +12,11 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git diy https://github.com/firker/diy-ziyong' feeds.conf.default
+# sed -i '$a src-git diy https://github.com/firker/diy-ziyong' feeds.conf.default
 # sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
 sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
 sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-SE'/g" ./package/base-files/files/bin/config_generate
 # sed -i "s/DISTRIB_DESCRIPTION='OpenWrt 18.06 by Lean '/DISTRIB_DESCRIPTION='OpenWrt '/g" ./package/lean/default-settings/files/zzz-default-settings
+git clone https://github.com/firker/diy-ziyong package/diy-ziyong
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
