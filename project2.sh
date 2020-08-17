c5b3d53b88e70375341c3d80d84e13c5802ffdb8
firker <56651759+firker@users.noreply.github.com>
2020-08-17 12:06:50 +0800

    Update project2.sh

--- project2.sh
+++ project2.sh
@@ -11,6 +11,7 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 rm -rf ./package/ctcgfw/luci-app-adguardhome
+rm -rf ./package/ctcgfw/luci-theme-argon-mod
 rm -rf ./package/ntlf9t/AdGuardHome
 rm -rf ./package/lean/luci-theme-argon
 rm -rf ./package/ntlf9t/luci-app-smartdns
