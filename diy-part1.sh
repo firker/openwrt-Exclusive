2602292a77184ee474d2dc0bfe3ec00c907e2e60
firker <56651759+firker@users.noreply.github.com>
2020-09-08 17:45:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,11 +25,12 @@ git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-ser
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 svn co https://github.com/firker/diy-ziyong/trunk/smartdns package/smartdns
 svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-passwall
-svn co https://github.com/xiaorouji/openwrt-package/trunk/package/brook package/brook
-svn co https://github.com/xiaorouji/openwrt-package/trunk/package/chinadns-ng package/chinadns-ng
-svn co https://github.com/xiaorouji/openwrt-package/trunk/package/tcping package/tcping
-svn co https://github.com/xiaorouji/openwrt-package/trunk/package/trojan-go package/trojan-go
-svn co https://github.com/xiaorouji/openwrt-package/trunk/package/trojan-plus package/trojan-plus
-svn co https://github.com/xiaorouji/openwrt-package/trunk/package/naiveproxy package/naiveproxy
-svn co https://github.com/xiaorouji/openwrt-package/trunk/package/v2ray package/v2ray
-svn co https://github.com/xiaorouji/openwrt-package/trunk/package/v2ray-plugin package/v2ray-plugin
+svn co https://github.com/xiaorouji/openwrt-package/trunk/package package/lienol
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/brook package/brook
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/chinadns-ng package/chinadns-ng
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/tcping package/tcping
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/trojan-go package/trojan-go
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/trojan-plus package/trojan-plus
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/naiveproxy package/naiveproxy
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/v2ray package/v2ray
+# svn co https://github.com/xiaorouji/openwrt-package/trunk/package/v2ray-plugin package/v2ray-plugin
