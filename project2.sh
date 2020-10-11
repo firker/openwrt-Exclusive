2ad4d9764601d3b8f229739c8610db4b2915b2ab
firker <56651759+firker@users.noreply.github.com>
2020-10-11 08:52:26 +0800

    Update project2.sh

--- project2.sh
+++ project2.sh
@@ -21,4 +21,6 @@ rm -rf ./package/ntlf9t/luci-app-smartdns
 rm -rf ./package/ntlf9t/smartdns
 rm -rf ./package/ntlf9t/openwrt-udp2raw
 rm -rf ./package/ntlf9t/dnscrypt-proxy-full
-curl -fsSL  https://raw.githubusercontent.com/Lienol/openwrt-packages/dev-19.07/net/https-dns-proxy/files/https-dns-proxy.init > ./feeds/packages/net/https-dns-proxy/files/https-dns-proxy.init
+# curl -fsSL  https://raw.githubusercontent.com/Lienol/openwrt-packages/dev-19.07/net/https-dns-proxy/files/https-dns-proxy.init > ./feeds/packages/net/https-dns-proxy/files/https-dns-proxy.init
+wget https://raw.githubusercontent.com/Lienol/openwrt-packages/dev-19.07/net/https-dns-proxy/files/https-dns-proxy.config -O ./feeds/packages/net/https-dns-proxy/files/https-dns-proxy.config
+wget https://raw.githubusercontent.com/Lienol/openwrt-packages/dev-19.07/net/https-dns-proxy/files/https-dns-proxy.init -O ./feeds/packages/net/https-dns-proxy/files/https-dns-proxy.init
