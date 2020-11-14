8f135668d56d0df32f986fbfa2b494b560a308fc
firker <56651759+firker@users.noreply.github.com>
2020-11-14 22:07:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,8 +19,8 @@ sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-SE'/g" ./package/base-files/files
 # curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/system.lua > ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-rm -rf ./package/lean/luci-theme-argon
 # rm -rf ./package/lean/dnsforwarder
+rm -rf ./package/lean/luci-theme-argon
 rm -rf ./feeds/packages/net/smartdns
 rm -rf ./feeds/packages/net/https-dns-proxy
 # rm -rf ./package/lean/v2ray
