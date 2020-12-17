0c2a837469cb68600985e490eacc2abd8252fc2e
firker <56651759+firker@users.noreply.github.com>
2020-12-17 12:11:29 +0800

    Update diy4.19.sh

--- diy4.19.sh
+++ diy4.19.sh
@@ -28,7 +28,7 @@ git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-svn co https://github.com/firker/diy-ziyong/trunk/smartdns package/smartdns
+git clone https://github.com/jerrykuku/luci-app-vssr.git  package/luci-app-vssr
 
 # svn co https://github.com/xiaorouji/openwrt-package/trunk/package/tcping package/tcping
 # svn co https://github.com/xiaorouji/openwrt-package/trunk/package/trojan-go package/trojan-go
