888821c8c63c9511446f46f086a3e8340065241f
firker <56651759+firker@users.noreply.github.com>
2021-01-15 22:10:08 +0800

    Add files via upload

---
+++ files/usr/share/JD_UP/up.sh
@@ -0,0 +1,9 @@
+# ------------------- 京东签到一键脚本！ ------------------- 
+
+
+npm install -g crypto-js got http-server tough-cookie download request tunnel
+
+git clone https://github.com/firkerword/JD_Script.git /usr/share/JD_Script
+
+chmod 777 /usr/share/JD_Script/jd.sh && sh /usr/share/JD_Script/jd.sh
+
