04bd41f0b1b6faf7258324fcff00538e5858d0c4
firker <56651759+firker@users.noreply.github.com>
2021-01-28 13:34:47 +0800

    Update jdup.sh

--- files/usr/share/JD_UP/jdup.sh
+++ files/usr/share/JD_UP/jdup.sh
@@ -3,7 +3,7 @@
 
 npm install -g crypto-js got http-server tough-cookie download request tunnel
 
-git clone https://github.com/firkerword/JD_Script.git /usr/share/JD_Script
+git clone -b main https://github.com/firkerword/JD_Script.git /usr/share/JD_Script
 
 chmod 777 /usr/share/JD_Script/jd.sh && sh /usr/share/JD_Script/jd.sh
 
