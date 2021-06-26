4e11c8e66f983442ff7fd02ea278f6d8531cfe03
firker <56651759+firker@users.noreply.github.com>
2021-06-26 19:37:24 +0800

    Update diy5.10.sh

--- diy5.10.sh
+++ diy5.10.sh
@@ -25,7 +25,7 @@ git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
 git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-git clone https://github.com/project-lede/luci-app-godproxy.git package/luci-app-godproxy
+# git clone https://github.com/project-lede/luci-app-godproxy.git package/luci-app-godproxy
 git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
-git clone https://github.com/BCYDTZ/luci-app-UUGameAcc.git package/luci-app-UUGameAcc
+# git clone https://github.com/BCYDTZ/luci-app-UUGameAcc.git package/luci-app-UUGameAcc
 # git clone https://github.com/ITdesk01/jd_openwrt_script.git package/jd_openwrt_script
