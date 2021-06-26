7c6e16e5ce6108fa8c7e78b6dff927951b6e5eb3
firker <56651759+firker@users.noreply.github.com>
2021-06-26 15:47:52 +0800

    Update diy5.10.sh

--- diy5.10.sh
+++ diy5.10.sh
@@ -18,7 +18,7 @@ sed -i '$a src-git ssr https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git diy https://github.com/firker/diy-ziyong' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
-sed -i '$a src-git bypass https://github.com/garypang13/openwrt-bypass' feeds.conf.default
+# sed -i '$a src-git bypass https://github.com/garypang13/openwrt-bypass' feeds.conf.default
 # mv -vf ../mt7621_phicomm_k2p.dts ./target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 #　git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
