a28339de586a0c38548e97d99422a4046b9b6080
firker <56651759+firker@users.noreply.github.com>
2021-07-02 12:49:40 +0800

    Update diy5.10.sh

--- diy5.10.sh
+++ diy5.10.sh
@@ -28,4 +28,4 @@ git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-ser
 # git clone https://github.com/project-lede/luci-app-godproxy.git package/luci-app-godproxy
 git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
 # git clone https://github.com/BCYDTZ/luci-app-UUGameAcc.git package/luci-app-UUGameAcc
-git clone https://github.com/yichya/luci-app-xray.git package/luci-app-xray
+# git clone https://github.com/yichya/luci-app-xray.git package/luci-app-xray
