ec9710a5c89809488c22e21e4637ab839a523f2c
firker <56651759+firker@users.noreply.github.com>
2022-02-25 19:36:30 +0800

    Update diy5.10.sh

--- diy5.10.sh
+++ diy5.10.sh
@@ -9,6 +9,7 @@
 #=============================================================
 
 sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
