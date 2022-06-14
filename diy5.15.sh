d178c3c28bcf2d18a9f2b3d294b3b7a6187fd7c6
firker <56651759+firker@users.noreply.github.com>
2022-06-14 22:28:35 +0800

    Update diy5.15.sh

--- diy5.15.sh
+++ diy5.15.sh
@@ -29,7 +29,7 @@ git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-
 # git clone https://github.com/kiddin9/openwrt-bypass.git package/openwrt-bypass
 git clone https://github.com/fw876/helloworld.git package/ssr
 git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
-# git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/passwall
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
