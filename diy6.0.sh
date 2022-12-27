000cca96f95698a5298272714b8d18cda92335c0
firkerword <61251923+firkerword@users.noreply.github.com>
2022-12-27 19:26:07 +0800

    Update diy6.0.sh

--- diy6.0.sh
+++ diy6.0.sh
@@ -20,7 +20,6 @@ sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.co
 # sed -i '$a src-git bypass https://github.com/garypang13/openwrt-bypass' feeds.conf.default
 # mv -vf ../mt7621_phicomm_k2p.dts ./target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 #　git clone https://github.com/vernesong/OpenClash.git package/OpenClash
-svn co https://github.com/fw876/helloworld/trunk/lua-neturl package/lua-neturl
 svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/aliyundrive-webdav
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
