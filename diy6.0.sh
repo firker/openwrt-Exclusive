a96712fcd2e291d101d69329a473a8478ebbb4fa
firkerword <61251923+firkerword@users.noreply.github.com>
2022-12-27 13:49:35 +0800

    Update diy6.0.sh

--- diy6.0.sh
+++ diy6.0.sh
@@ -26,11 +26,11 @@ svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/l
 svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
 # svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
 # svn co https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
-git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
+# git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
 git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
 git clone https://github.com/sbwml/openwrt-alist.git package/openwrt-alist
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
-# git clone https://github.com/fw876/helloworld.git package/ssr
+git clone https://github.com/fw876/helloworld.git package/ssr
 git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
 git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 # git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
