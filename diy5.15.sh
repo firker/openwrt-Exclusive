715620a4d52086f15c254d6ddea0fd7abfefb0db
firkerword <61251923+firkerword@users.noreply.github.com>
2023-01-14 12:42:31 +0800

    Update diy5.15.sh

--- diy5.15.sh
+++ diy5.15.sh
@@ -8,7 +8,7 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
 
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
