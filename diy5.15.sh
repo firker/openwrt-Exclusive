7112528d9ab3eed03f9c92a3485b4813e09a647d
xsoloking <soloking.x@gmail.com>
2023-04-06 22:51:26 +0800

    Update diy5.15.sh

--- diy5.15.sh
+++ diy5.15.sh
@@ -8,40 +8,40 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.19/g' ./target/linux/x86/Makefile
 
 # Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/#src-/src-/g' feeds.conf.default
 
-# Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-# sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
-# sed -i '$a src-git bypass https://github.com/garypang13/openwrt-bypass' feeds.conf.default
-# mv -vf ../mt7621_phicomm_k2p.dts ./target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-#　git clone https://github.com/vernesong/OpenClash.git package/OpenClash
-svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/aliyundrive-webdav
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
-# svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
-# svn co https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
+# # Add a feed source
+# # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# # sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+# # sed -i '$a src-git bypass https://github.com/garypang13/openwrt-bypass' feeds.conf.default
+# # mv -vf ../mt7621_phicomm_k2p.dts ./target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+# #　git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+# svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/aliyundrive-webdav
+# svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+# # svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
+# # svn co https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
+# # git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
+# git clone https://github.com/sbwml/openwrt-alist.git package/openwrt-alist
+# git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
+# svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
+# git clone https://github.com/fw876/helloworld.git package/ssr
+# git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
+# git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+# git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/passwall
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06
+# git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
+# git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
+# git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+# git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 # git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
-git clone https://github.com/sbwml/openwrt-alist.git package/openwrt-alist
-git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
-git clone https://github.com/fw876/helloworld.git package/ssr
-git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
-git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/passwall
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06
-git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
-git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
-git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
-git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
-git clone https://github.com/firkerword/luci-app-lucky.git package/lucky
-# git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
-# git clone https://github.com/project-lede/luci-app-godproxy.git package/luci-app-godproxy
-# git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
-# git clone https://github.com/BCYDTZ/luci-app-UUGameAcc.git package/luci-app-UUGameAcc
-# git clone https://github.com/yichya/luci-app-xray.git package/luci-app-xray
- find ./ | grep Makefile | grep xray-plugin | xargs rm -f
-svn co https://github.com/sbwml/openwrt_helloworld/trunk/xray-plugin package/xray-plugin
+# git clone https://github.com/firkerword/luci-app-lucky.git package/lucky
+# # git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
+# # git clone https://github.com/project-lede/luci-app-godproxy.git package/luci-app-godproxy
+# # git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
+# # git clone https://github.com/BCYDTZ/luci-app-UUGameAcc.git package/luci-app-UUGameAcc
+# # git clone https://github.com/yichya/luci-app-xray.git package/luci-app-xray
+#  find ./ | grep Makefile | grep xray-plugin | xargs rm -f
+# svn co https://github.com/sbwml/openwrt_helloworld/trunk/xray-plugin package/xray-plugin
