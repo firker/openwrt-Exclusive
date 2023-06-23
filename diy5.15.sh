a998d94182def3ca7a93bf29a25d6604fad1b37a
firker <56651759+firker@users.noreply.github.com>
2023-06-23 14:03:13 +0800

    Update diy5.15.sh

--- diy5.15.sh
+++ diy5.15.sh
@@ -24,7 +24,7 @@ svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/l
 # svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
 # svn co https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
 # git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
-git clone https://github.com/sbwml/openwrt-alist.git package/openwrt-alist
+# git clone https://github.com/sbwml/openwrt-alist.git package/openwrt-alist
 git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
 git clone https://github.com/fw876/helloworld.git package/ssr
