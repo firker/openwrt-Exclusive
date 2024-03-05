1a92ff2f6a9762746a6f6616d37023c723e959f1
firkerword <61251923+firkerword@users.noreply.github.com>
2024-03-05 18:05:56 +0800

    Update diy6.0.sh

--- diy6.0.sh
+++ diy6.0.sh
@@ -43,13 +43,13 @@ merge_package https://github.com/sirpdboy/sirpdboy-package sirpdboy-package/smar
 git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
 # git clone https://github.com/sbwml/openwrt-alist.git package/openwrt-alist
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
-git clone https://github.com/fw876/helloworld.git package/ssr
+# git clone https://github.com/fw876/helloworld.git package/ssr
 git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall
 # git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 # git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
 # git clone https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
-git clone -b luci-smartdns-new-version https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+# git clone -b luci-smartdns-new-version https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
 git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
